89e47d411afba237a0b78bee97203d4ab28fccd0
Yoann Leblanc <yoannleblanc88@gmail.com>
2025-03-06 17:01:29 +0100

    rails watch list in progress

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+#   def destroy
+#     @bookmark = Bookmark.find(params[:id])
+#     @bookmark.destroy
+#     redirect_to lists_path, status: :see_other
+#   end
+# end
+
+private
+def bookmark_params
+  params.require(:bookmark).permit(:comment)
+end
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, :overview, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  before_destroy :ensure_no_children
+
+  private
+
+  def ensure_no_children
+    raise.ActiveRecord::InvalidForeignKey unless self.bookmarks
+  end
 end
